772f87bee81ca91053b216ca31f72455972b180d
TechyRajat8980 <rajatdubey395@gmail.com>
2024-01-04 19:37:09 +0530

    added datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,23 @@
+public class datatypes {
+    public static void main(String[] args) {
+        // syntax <data type> <variable_name>
+        // <data type> <variable_name> = value;
+        // primitive = self derived = pracheen 
+        // boolean, char, byte, short, int, long, float, double
+        boolean bl = true;
+        char ch = 'A';
+        byte by = 123;
+        int intData = 12345;
+        float fl = 12345.19f;
+        double dl = 12345.19;
+        System.out.println(bl);
+        System.out.println(ch);
+        System.out.println(by);
+        System.out.println(intData);
+        System.out.println(fl);
+        System.out.println(dl);
+
+
+
+    }
+}
