a4eb4950f7289bae3c78f1135bec6248abde75be
TechyRajat8980 <rajatdubey395@gmail.com>
2024-01-12 16:30:19 +0530

    userinput

---
+++ UserInput/UserInput.java
@@ -0,0 +1,13 @@
+package UserInput;
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the 1st value: ");
+        int num1 = scan.nextInt();
+        System.out.println("Enter the 2nd value: ");
+        int num2 = scan.nextInt();
+        System.out.println("Sum = "+(num1+num2));
+    }
+}
