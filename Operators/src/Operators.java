63fbb6623d9b6128d65f44b99b64b596e6c25c19
TechyRajat8980 <rajatdubey395@gmail.com>
2024-02-13 22:08:17 +0530

    This is operators

--- Operators/src/Operators.java
+++ Operators/src/Operators.java
@@ -1,8 +1,16 @@
 public class Operators {
     public static void main(String[] args) {
-        int num1 = 3;
-        int num2 = 4;
-        int rslt = num1%num2;
-        System.out.println(rslt);
+//        assignment operator
+        int num = 3;
+//        int num2 = 4;
+//        int rslt = num1%num2;
+//        num1 += 2;
+//        num1++;
+//        num--; post - increment
+//        ++num; pre - increment
+        int result = num++;
+//        first fetch the value then increment
+//        int result = ++num; [first increment then fetch the value]
+        System.out.println(result);
     }
 }
